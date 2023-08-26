8b61dc4f3d80621cc82d41ff62cef5efe897fccf
Sazzad Hossain <93114191+Sazzad-Hossain-Sobuj@users.noreply.github.com>
2023-08-26 20:41:48 +0600

    module 4 assignment

---
+++ module4/module4_all/module4_assignment/class.dart
@@ -0,0 +1,48 @@
+abstract class Account {
+  int accountNumber;
+  double balance;
+
+  Account(this.accountNumber, this.balance);
+
+  void deposit(double amount) {
+    balance += amount;
+    print('$amount taka is deposited. \nNow the current balance is: $balance');
+  }
+
+  void withdraw(double amount);
+}
+
+class SavingsAccount extends Account {
+  double interestRate;
+
+  SavingsAccount(int accountNumber, double balance, this.interestRate)
+      : super(accountNumber, balance);
+
+  @override
+  void withdraw(double amount) {
+    if (amount <= balance) {
+      balance -= amount;
+      balance += balance * interestRate;
+      print('Withdrawn from Savings: $amount \nNow the New Balance: $balance');
+    } else {
+      print('Insufficient funds');
+    }
+  }
+}
+
+class CurrentAccount extends Account {
+  double overdraftLimit;
+
+  CurrentAccount(int accountNumber, double balance, this.overdraftLimit)
+      : super(accountNumber, balance);
+
+  @override
+  void withdraw(double amount) {
+    if (amount <= balance + overdraftLimit) {
+      balance -= amount;
+      print('Withdrawn from Current: $amount \nCurrent Balance: $balance');
+    } else {
+      print('Insufficient funds');
+    }
+  }
+}
--- module4/module4_all/module4_assignment/module4_assignment.dart
+++ module4/module4_all/module4_assignment/module4_assignment.dart
@@ -1,3 +1,12 @@
-void main(){
+import 'class.dart';
+void main() {
+  SavingsAccount savings = SavingsAccount(100123, 50000.0, 0.1);
+  print('Savings Account:');
+  savings.deposit(500.0);
+  savings.withdraw(200.0);
 
+  CurrentAccount current = CurrentAccount(100456, 20000.0, 1000.0);
+  print('\nCurrent Account:');
+  current.deposit(1000.0);
+  current.withdraw(6000.0);
 }
